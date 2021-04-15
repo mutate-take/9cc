f8447c160984f4225668d96818d7e107fd063200
mutate-take <tip12128@gmail.com>
2021-04-15 23:59:45 +0900

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -1,16 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char** argv){
-    if (argc != 2) {
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
 
+    char *p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("\tmov rax, %d\n", atoi(argv[1]));
+    printf("\tmov rax, %ld\n", strtol(p, &p, 10));
+
+
+
+    while (*p)
+    {
+        if (*p == '+')
+        {
+            p++;
+            printf("\tadd rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-')
+        {
+            p++;
+            printf("\tsub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字です : '%c'\n", *p);
+        return 1;
+    }
+
     printf("\tret\n");
     return 0;
 }
